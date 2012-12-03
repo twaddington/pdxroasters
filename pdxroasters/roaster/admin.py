4371f6f856a65b533358a3134a525520d3c03972
Tristan Waddington <tristan.waddington@gmail.com>
2012-12-02 18:19:31 -0800

    Update the model admin list fields and search options.

--- pdxroasters/roaster/admin.py
+++ pdxroasters/roaster/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from roaster.models import Cafe, Roaster, Roast
 
-admin.site.register(Cafe)
-admin.site.register(Roaster)
-admin.site.register(Roast)
+class CafeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'address', 'phone', 'url', 'created_at', 'modified_at',)
+    search_fields = ('name',)
+
+class RoasterAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'address', 'phone', 'url', 'created_at', 'modified_at',)
+    search_fields = ('name',)
+
+class RoastAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'modified_at',)
+    search_fields = ('name',)
+
+admin.site.register(Cafe, CafeAdmin)
+admin.site.register(Roaster, RoasterAdmin)
+admin.site.register(Roast, RoastAdmin)
