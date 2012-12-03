7af7d6e79cc5ffcc8d099754e1b118137639f01d
Tristan Waddington <tristan.waddington@gmail.com>
2012-12-02 18:09:01 -0800

    Add model unicode methods; populate slug field on save.

--- pdxroasters/roaster/models.py
+++ pdxroasters/roaster/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Cafe(models.Model):
-    slug = models.SlugField()
     name = models.CharField(max_length=200, unique=True, db_index=True,
             blank=False)
+    slug = models.SlugField()
     address = models.CharField(max_length=200)
     #hours
     phone = models.CharField(max_length=10)
@@ -11,13 +12,22 @@ class Cafe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     modified_at = models.DateTimeField(auto_now=True, db_index=True)
  
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name)
+
+        super(Cafe, self).save(*args, **kwargs)
+
     class Meta:
         ordering = ['name',]
 
 class Roaster(models.Model):
-    slug = models.SlugField()
     name = models.CharField(max_length=200, unique=True, db_index=True,
             blank=False)
+    slug = models.SlugField()
     address = models.CharField(max_length=200)
     # TODO: Hours
     phone = models.CharField(max_length=10)
@@ -29,6 +39,15 @@ class Roaster(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     modified_at = models.DateTimeField(auto_now=True, db_index=True)
 
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.name)
+
+        super(Roaster, self).save(*args, **kwargs)
+
     class Meta:
         ordering = ['name',]
         get_latest_by = 'created_at'
@@ -39,7 +58,10 @@ class Roast(models.Model):
     roaster = models.ForeignKey('Roaster')
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     modified_at = models.DateTimeField(auto_now=True, db_index=True)
-    
+
+    def __unicode__(self):
+        return self.name
+
     class Meta:
         ordering = ['name',]
 
