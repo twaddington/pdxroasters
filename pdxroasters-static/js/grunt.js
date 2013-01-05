08f8cffac4d69baa7140c81540c8d0e18a24556f
Brandon Kitajchuk <brandonkitajchuk@Brandon-Kitajchuk-2.local>
2013-01-04 20:42:05 -0800

    adding grunt js to project

---
+++ pdxroasters-static/js/grunt.js
@@ -0,0 +1,64 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! PDX Roasters - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://PROJECT_WEBSITE/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'PDX Roasters; Licensed MIT */'
+    },
+    lint: {
+      files: [
+        'vendor/*',
+        'lib/*'
+    ]
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:lint.files>'],
+        dest: 'dist/scripts.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/scripts.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'concat min'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+  grunt.registerTask('build', 'concat min');
+
+};
