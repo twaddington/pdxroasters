f1879a1f476726f9729e1b86d89fdb4653aaa613
Brandon Kitajchuk <brandonkitajchuk@Brandon-Kitajchuk-3.local>
2013-04-01 14:51:51 -0700

    working on updates to pushstate api

--- site/pdxroasters/static/js/lib/nav.js
+++ site/pdxroasters/static/js/lib/nav.js
@@ -9,6 +9,12 @@
 
 "use strict";
 
+// Closure globals
+var $_document = $( document ),
+    $_body = $( document.body ),
+    $_window = $( window ),
+    $_content = $( "#content" );
+
 window.pdx.nav = {
 	init: function () {
 		var self = this;
@@ -16,18 +22,34 @@ window.pdx.nav = {
         this.$nav = $( "#nav" );
         this.$navTog = this.$nav.find( ".plus" );
         this.$navLinks = this.$nav.find( "a:not(.plus)" );
-        this.$pages = $( "#info-pages" );
-        this.$activePage;
-        this.$lastPage;
-        this.activePage;
-        this.lastPage;
+        this.$pushPage = $( "#nav-push-page" );
         this.isPopStateEvent = false;
-        this.pushState = new window.pdx.PushState({
+        this.isOnLoad = true;
+        this.pushState = window.pdx.pushstate({
 	        async: false
         });
         
-        this.pushState.onpop(function ( back, forward ) {
-        	// Figure this out...
+        this.pushState.onpop(function ( state ) {
+        	if ( self.isOnLoad ) {
+        		self.isOnLoad = false;
+        		return;
+        	}
+        	
+        	self.isPopStateEvent = true;
+        	
+        	var path = state.to
+        		.replace( location.protocol, "" )
+        		.replace( location.host, "" )
+        		.replace( /\//g, "" );
+        	
+        	if ( path === "" && !self.isOnLoad ) {
+        		self.$navTog.click();
+        		
+        	} else {
+	        	self.$nav.find( "[href*='"+path+"']" ).click();
+        	}
+        	
+        	self.isPopStateEvent = false;
         });
         
         this.$navTog.on( "click", function ( e ) {
@@ -37,10 +59,10 @@ window.pdx.nav = {
             self.$nav.toggleClass( "active" );
             
             if ( !self.$navTog.is( ".active" ) ) {
-            	self.$pages.removeClass( "active" );
+            	self.$pushPage.removeClass( "active" );
             	
             } else {
-                $( "[data-page='"+self.activePage+"']" ).click();
+                //$( "[data-page='"+self.activePage+"']" ).click();
             }
             
             self.pushState.push( "/" );
@@ -49,30 +71,22 @@ window.pdx.nav = {
         this.$navLinks.on( "click", function ( e ) {
             e.preventDefault();
             
-            if ( !self.$pages.is( ".active" ) ) {
-            	self.$pages.addClass( "active" );
+            if ( !self.$pushPage.is( ".active" ) ) {
+            	self.$pushPage.addClass( "active" );
             }
             
             var $this = $( this ),
-            	page = $this.data( "page" );
-            
-            self.lastPage = self.activePage;
-            self.$lastPage = self.$activePage;
+            	path = this.href
+            		.replace( location.protocol, "" )
+            		.replace( location.host, "" )
+            		.replace( /\//g, "" ),
+            	$page = $( "#"+path );
             
             self.$navLinks.removeClass( "on" );
             $this.addClass( "on" );
             
-            self.$activePage = $( "#"+page );
-            self.activePage = page;
-            
-            if ( self.$lastPage && self.$lastPage.index() > self.$activePage.index() ) {
-            	self.$lastPage.css( "left", "100%" );
-            	
-            } else if ( self.$lastPage && self.$lastPage.index() < self.$activePage.index() ) {
-	            self.$lastPage.css( "left", "-100%" );
-            }
-            
-            self.$activePage.css( "left", 0 );
+            self.$pushPage.find( ".page" ).removeClass( "active" );
+            $page.addClass( "active" );
             
             if ( !self.isPopStateEvent ) {
             	self.pushState.push( this.href );
