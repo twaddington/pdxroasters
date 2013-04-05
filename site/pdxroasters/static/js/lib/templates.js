90d7a955b1169af78155d75929955e727c0f5173
Brandon Kitajchuk <brandonkitajchuk@Brandon-Kitajchuk-3.local>
2013-04-05 16:05:54 -0700

    removing mustache js as its not really needed so cutting the weight

--- site/pdxroasters/static/js/lib/templates.js
+++ site/pdxroasters/static/js/lib/templates.js
@@ -28,38 +28,33 @@
 
 "use strict";
 
-// Closure global
-var mustache = require( "mustache" );
-
 // Templates namespace
 window.pdx.templates = {
     infowindow: function ( view ) {
         var tmpl = "";
         
-        tmpl = '<h3>{{ name }}</h3>';
+        tmpl = '<h3>'+view.name+'</h3>';
         tmpl += '<div class="group">';
             tmpl += '<div class="col col1of2">';
-                tmpl += '<div class="ci">{{ address }}</div>';
+                tmpl += '<div class="ci">'+view.address+'</div>';
             tmpl += '</div>';
             tmpl += '<div class="col col1of2">';
                 tmpl += '<p class="hours ci">';
-                    tmpl += 'Mon. - Fri. <span>8 - 12</span><br />';
-                    tmpl += 'Sat. <span>8 - 12</span><br />';
-                    tmpl += 'Sun. <span>8 - 12</span>';
+                    tmpl += 'TBD:HOURS';
                 tmpl += '</p>';
             tmpl += '</div>';
         tmpl += '</div>';
         tmpl += '<div class="btns group">';
             tmpl += '<div class="col col1of2">';
-                tmpl += '<div class="ci"><a href="#{{ id }}" class="btn find">Find this Roast</a></div>';
+                tmpl += '<div class="ci"><a href="#'+view.id+'" class="btn find">Find this Roast</a></div>';
             tmpl += '</div>';
             tmpl += '<div class="col col1of2">';
-                tmpl += '<div class="ci"><a href="/roaster/{{ slug }}/" class="btn more">Learn More</a></div>';
+                tmpl += '<div class="ci"><a href="/roaster/'+view.slug+'/" class="btn more">Learn More</a></div>';
             tmpl += '</div>';
         tmpl += '</div>';
         tmpl += '<a href="#close" class="plus-close">Close</div>';
         
-        return mustache.render( tmpl, view );
+        return tmpl;
     }
 }
 
