0430370fb71a2932890d35dc429861e8bc1f0c9c
Paul C Pederson <ppederson@esri.com>
2013-05-15 22:08:33 -0700

    restyled modal, simple

--- site/pdxroasters/static/js/lib/templates.js
+++ site/pdxroasters/static/js/lib/templates.js
@@ -13,25 +13,25 @@
 window.pdx.templates = {
     roasterInfo: function ( view ) {
         var html = "";
-        
-        html += '<a href="#'+view.id+'" class="btn find">Find this Roast</a>';
+
+        html += '<a href="#'+view.id+'" class="find"></a>';
         html += '<h3>'+view.name+'</h3>';
-        html += '<a href="#'+view.id+'" class="btn more">Learn More</a>';
-        
+        html += '<a href="#'+view.id+'" class="more"></a>';
+
         return html;
     },
-    
+
     cafeInfo: function ( view ) {
         var html = "";
-        
+
         html = '<h3>'+view.name+'</h3>';
         if ( window.pdx.maps.location.isset ) {
         	html += '<a href="'+window.pdx.maps.url+'?daddr='+view.lat+','+view.lng+'&saddr='+window.pdx.maps.location.lat+','+window.pdx.maps.location.lng+'" class="btn more" target="_blank">Get Directions</a>';
-        	
+
         } else {
             html += '<a href="'+window.pdx.maps.url+'?daddr='+view.lat+','+view.lng+'" class="btn more" target="_blank">Get Directions</a>';
         }
-        
+
         return html;
     }
 }
