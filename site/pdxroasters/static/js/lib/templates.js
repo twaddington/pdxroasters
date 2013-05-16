e42aed5388075cd953095176fc1a4fd0c0a23404
Brandon Kitajchuk <brandonkitajchuk@Brandon-Kitajchuk-3.local>
2013-05-15 20:05:36 -0700

    adding new infowindow style, fixing nav-push-pages bug

--- site/pdxroasters/static/js/lib/templates.js
+++ site/pdxroasters/static/js/lib/templates.js
@@ -14,26 +14,9 @@ window.pdx.templates = {
     roasterInfo: function ( view ) {
         var html = "";
         
-        html = '<h3>'+view.name+'</h3>';
-        html += '<div class="group">';
-            html += '<div class="col col1of2">';
-                html += '<div class="ci">'+view.address+'</div>';
-            html += '</div>';
-            html += '<div class="col col1of2">';
-                html += '<p class="hours ci">';
-                    html += this._getRoasterHours( view.id );
-                html += '</p>';
-            html += '</div>';
-        html += '</div>';
-        html += '<div class="btns group">';
-            html += '<div class="col col1of2">';
-                html += '<div class="ci"><a href="#'+view.id+'" class="btn find">Find this Roast</a></div>';
-            html += '</div>';
-            html += '<div class="col col1of2">';
-                html += '<div class="ci"><a href="#'+view.id+'" class="btn more">Learn More</a></div>';
-            html += '</div>';
-        html += '</div>';
-        html += '<a href="#close" class="plus-close">Close</div>';
+        html += '<a href="#'+view.id+'" class="btn find">Find this Roast</a>';
+        html += '<h3>'+view.name+'</h3>';
+        html += '<a href="#'+view.id+'" class="btn more">Learn More</a>';
         
         return html;
     },
@@ -42,32 +25,14 @@ window.pdx.templates = {
         var html = "";
         
         html = '<h3>'+view.name+'</h3>';
-        html += '<div class="group">';
-        	html += '<div class="col col1of2">';
-        		html += view.address+'<br />';
-        		html += view.phone+'<br />';
-        		html += '<a href="'+view.url+'" target="_blank">Website</a>';
-        	html += '</div>';
-        html += '</div>';
-        html += '<div class="btns group">';
-        	html += '<div class="col col1of2">';
-                html += '<div class="ci">';
-                if ( window.pdx.maps.location.isset ) {
-                	html += '<a href="'+window.pdx.maps.url+'?daddr='+view.lat+','+view.lng+'&saddr='+window.pdx.maps.location.lat+','+window.pdx.maps.location.lng+'" class="btn more" target="_blank">Get Directions</a>';
-                	
-                } else {
-	                html += '<a href="'+window.pdx.maps.url+'?daddr='+view.lat+','+view.lng+'" class="btn more" target="_blank">Get Directions</a>';
-                }
-                html += '</div>';
-            html += '</div>';
-        html += '</div>';
-        html += '<a href="#close" class="plus-close">Close</div>';
+        if ( window.pdx.maps.location.isset ) {
+        	html += '<a href="'+window.pdx.maps.url+'?daddr='+view.lat+','+view.lng+'&saddr='+window.pdx.maps.location.lat+','+window.pdx.maps.location.lng+'" class="btn more" target="_blank">Get Directions</a>';
+        	
+        } else {
+            html += '<a href="'+window.pdx.maps.url+'?daddr='+view.lat+','+view.lng+'" class="btn more" target="_blank">Get Directions</a>';
+        }
         
         return html;
-    },
-    
-    _getRoasterHours: function ( id ) {
-	    return $( "#roaster-"+id+"-hours" ).html();
     }
 }
 
