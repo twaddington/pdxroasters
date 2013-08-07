1a79747c47c4ff291bea43885ed5ee3bae2dcc31
Paul C Pederson <ppederson@esri.com>
2013-08-06 21:01:27 -0700

    added grunt, cleaned up gitignore

---
+++ site/pdxroasters/static/Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      scripts: {
+        files: ['./**/*.js','./**/*.scss'],
+        tasks: ['compass','jshint'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    compass: {
+      dev: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css'
+        }
+      }
+    },
+    jshint: {
+      files: ['./*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Default task(s)
+  grunt.registerTask('default', ['watch']);
+
+
+};
