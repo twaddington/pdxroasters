5163694fa0faaf74530c91bd84dae157f9e5c241
Paul C Pederson <ppederson@esri.com>
2013-09-10 17:05:17 -0700

    swap out map

--- site/pdxroasters/static/js/map.js
+++ site/pdxroasters/static/js/map.js
@@ -23,7 +23,7 @@ define(["Leaflet"], function (L) {
 
   var Map = {
     init: function() {
-      L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+      L.tileLayer('http://{s}.tiles.mapbox.com/v3/financialtimes.map-w7l4lfi8/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
       }).addTo(map);
